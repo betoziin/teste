452f48f786bf832edddddd8031bcbaa2a2d39dce
betoziin <88126700+betoziin@users.noreply.github.com>
2021-07-29 00:42:55 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,52 @@
+
+const help = (prefix) => { 
+	return `  
+ COMANDO BOT
+
+    *${prefix}base1*
+    Uso: !base1 
+    BASE 1 CPF 000 - 009
+    
+    *${prefix}base2*
+    Uso: !base2 
+    BASE 2 CPF 009 - 017
+    
+    *${prefix}base3*
+    Uso: !base3 
+    BASE 3 CPF 017 - 026
+    
+    *${prefix}base4*
+    Uso: !base4
+    BASE 4 CPF 026 - 034
+    
+    *${prefix}base5*
+    Uso: !base5
+    BASE 5 CPF 034 - 043
+    
+    *${prefix}base6*
+    Uso: !base6
+    BASE 6 CPF 043 - 054
+    
+    *${prefix}base7*
+    Uso: !base7
+    BASE 7 CPF 054 - 061
+    
+    *${prefix}base8*
+    Uso: !base8
+    BASE 8 CPF 061 - 073
+    
+    *${prefix}base9*
+    Uso: !base9
+    BASE 9 CPF 073 - 084
+
+    *${prefix}base10*
+    Uso: !base10
+    BASE 10 CPF 084 - 097
+
+`
+}
+exports.help = help
+
+
+
+  
